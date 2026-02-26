98fa30a2c9249c8c99c21bb8de2bab8243b29d14
Diego Perez <145170018+Diego3128@users.noreply.github.com>
2026-02-25 19:17:31 -0500

    initial commit - folder structure - routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { FullscreenMapPage } from './pages/fullscreen-map-page/fullscreen-map-page';
+import { MarkersPage } from './pages/markers-page/markers-page';
+import { HousesPage } from './pages/houses-page/houses-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'fullscreen',
+    component: FullscreenMapPage,
+    title: 'full screen map'
+  },
+  {
+    path: 'marker',
+    component: MarkersPage,
+    title: 'your markers'
+  },
+  {
+    path: 'houses',
+    component: HousesPage,
+    title: 'available properties'
+  },
+  {
+    path: '**',
+    redirectTo: 'fullscreen'
+  }
+];
---
+++ src/app/shared/components/navbar/navbar.ts
@@ -0,0 +1,32 @@
+import { Component, inject } from '@angular/core';
+import { routes } from '../../../app.routes';
+import { NavigationEnd, Router, RouterLink, RouterLinkActive } from "@angular/router";
+import { filter, map } from 'rxjs';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navbar.html',
+})
+export class Navbar {
+
+  routerService = inject(Router);
+
+  routes = routes
+    .map(r => ({ path: `${r.path ?? ''}`, title: `${r.title ?? ''}` }))
+    .filter(r => r.path !== "**");
+
+
+  currentUrl$ = this.routerService.events.pipe(
+    filter(event => event instanceof NavigationEnd),
+    map((value) => value.urlAfterRedirects),
+    map(url => url.replace("/", "")),
+    map((url) => this.routes.find(route => route.path === url)?.title ?? ''),
+    takeUntilDestroyed()
+  )
+
+  currentPath = toSignal(this.currentUrl$, { initialValue: '' });
+
+
+}
